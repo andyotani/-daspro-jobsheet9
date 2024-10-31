f1fc0c0891d3633a5233d1d2a9c2adec6070ff33
andyotani <andidipo6@gmail.com>
2024-10-31 09:34:45 +0700

    commit

--- ArrayRataNilai04pertanyaan.java
+++ ArrayRataNilai04pertanyaan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ArrayNilai04pertanyaan {
+public class ArrayRataNilai04pertanyaan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
