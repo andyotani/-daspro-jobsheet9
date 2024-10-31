e1b328cad6a84c275e6c4b6624cf250865f4b73d
andyotani <andidipo6@gmail.com>
2024-10-31 09:26:17 +0700

    commit

--- ArrayNilai04.java
+++ ArrayNilai04.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
+
 public class ArrayNilai04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
+        int jumlahLulus = 0;
 
-        for(int i = 0; i < 10; i++){
-            System.out.print("Masukan nilai akhir ke-"+ i +" : ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
-        for(int i = 0; i < 10; i++){
-            System.out.println("Nilai akhir ke-"+ i +" adalah "+nilaiAkhir[i]);
-            
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70) {
+                jumlahLulus++;
+            }
         }
+        
+        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahLulus);
     }
 }
---
+++ ArrayNilai04pertanyaan.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ArrayNilai04pertanyaan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+        int[] nilaiAkhir = new int[jumlahMahasiswa];
+
+        int jumlahLulus = 0;
+        int totalNilaiLulus = 0;
+        int jumlahTidakLulus = 0;
+        int totalNilaiTidakLulus = 0;
+
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiAkhir[i] = sc.nextInt();
+
+            if (nilaiAkhir[i] > 70) {
+                totalNilaiLulus += nilaiAkhir[i];
+                jumlahLulus++;
+            } else {
+                totalNilaiTidakLulus += nilaiAkhir[i];
+                jumlahTidakLulus++;
+            }
+        }
+
+        double rataRataLulus = jumlahLulus > 0 ? (double) totalNilaiLulus / jumlahLulus : 0;
+        double rataRataTidakLulus = jumlahTidakLulus > 0 ? (double) totalNilaiTidakLulus / jumlahTidakLulus : 0;
+
+        System.out.println("Rata-rata nilai lulus = " + rataRataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataRataTidakLulus);
+    }
+}
