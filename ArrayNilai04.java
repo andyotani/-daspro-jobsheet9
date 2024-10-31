cfb8ddb8107c60b0b1474281a16c3ce212ddb46e
andyotani <andidipo6@gmail.com>
2024-10-31 10:30:01 +0700

    commit

--- ArrayNilai04.java
+++ ArrayNilai04.java
@@ -4,20 +4,18 @@ public class ArrayNilai04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        int jumlahLulus = 0;
-
+        
         for (int i = 0; i < 10; i++) {
-            System.out.print("Masukan nilai akhir ke-" + i + " : ");
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-
+        
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
             if (nilaiAkhir[i] > 70) {
-                jumlahLulus++;
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
             }
         }
-        
-        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahLulus);
     }
 }
