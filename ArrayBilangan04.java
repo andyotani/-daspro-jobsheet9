0c956955483b7869bf281ec6dcb297b93ae3b889
andyotani <andidipo6@gmail.com>
2024-10-31 07:58:48 +0700

    commit

--- ArrayBilangan04.java
+++ ArrayBilangan04.java
@@ -9,6 +9,9 @@ public class ArrayBilangan04 {
         bil[2] = -7;
         bil[3] = 17;
         
+        for (int i = 0; i < 4; i++){
+            System.out.println(bil[i]);
+        }
         System.out.println(bil[0]);
         System.out.println(bil[1]);
         System.out.println(bil[2]);
